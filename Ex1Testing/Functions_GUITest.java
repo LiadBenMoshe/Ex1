b8f375d44712a03aa1a80d070a8eacbeab5f251c
liad <liadbenmoshe10@gmail.com>
2019-12-09 15:32:56 +0200

    gui testing

--- Ex1Testing/Functions_GUITest.java
+++ Ex1Testing/Functions_GUITest.java
@@ -1,15 +1,25 @@
 package Ex1Testing;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 import org.junit.jupiter.api.Test;
 
 import Ex1.ComplexFunction;
 import Ex1.Functions_GUI;
 import Ex1.Monom;
+import Ex1.Operation;
 import Ex1.Polynom;
 import Ex1.Range;
 import Ex1.function;
+import Ex1.functions;
 /**
+ * Note: minor changes (thanks to Amichach Hadad!!)
+ * The use of "get" was replaced by iterator!
+ * 
  * Partial JUnit + main test for the GUI_Functions class, expected output from the main:
  * 0) java.awt.Color[r=0,g=0,b=255]  f(x)= plus(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x +5.0)
 1) java.awt.Color[r=0,g=255,b=255]  f(x)= plus(div(+1.0x +1.0,mul(mul(+1.0x +3.0,+1.0x -2.0),+1.0x -4.0)),2.0)
@@ -24,50 +34,56 @@ import Ex1.function;
  */
 class Functions_GUITest {
 	public static void main(String[] a) {
-		Functions_GUI data = FunctionsFactory();
-		int w=1000, h=600, res=200;
-		Range rx = new Range(-10,10);
-		Range ry = new Range(-5,15);
-		data.drawFunctions(w,h,rx,ry,res);
-	}
-	private Functions_GUI _data=null;
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//	}
+		functions data = FunctionsFactory();
+		String file = "function_file.txt";
+		String file2 = "function_file2.txt";
+		try {
+			data.saveToFile(file);
+			Functions_GUI data2 = new Functions_GUI();
+			data2.initFromFile(file);
+			data2.saveToFile(file2);
 
-	@BeforeEach
-	void setUp() throws Exception {
-		_data = FunctionsFactory();
+		}
+		catch(Exception e) {e.printStackTrace();}
 	}
 
-	//@Test
-	void testFunctions_GUI() {
-	//	fail("Not yet implemented");
-	}
 
-	//@Test
-	void testInitFromFile() {
-	//	fail("Not yet implemented");
-	}
-
-	//@Test
-	void testSaveToFile() {
-	//	fail("Not yet implemented");
-	}
-
-	//@Test
-	void testDrawFunctions() {
-		//_data.drawFunctions();
-	//	fail("Not yet implemented");
+	@Test
+	void testInitFromFile() throws IOException {
+		Functions_GUI fg1=new Functions_GUI();
+		fg1.initFromFile("function_file.txt");
+		assertTrue(!fg1.isEmpty());
 	}
 
 	@Test
-	void testDrawFunctionsIntIntRangeRangeInt() {
-		_data.drawFunctions();
-		//fail("Not yet implemented");
+	void testSaveToFile() throws IOException {
+		Functions_GUI fg1=new Functions_GUI();
+		fg1.initFromFile("function_file.txt");
+		fg1.saveToFile("function_file2.txt");
+
 	}
-	public static Functions_GUI FunctionsFactory() {
-		Functions_GUI ans = new Functions_GUI();
+
+	
+	@Test
+	void testDrawFunctions() throws IOException {
+		Functions_GUI fg1=new Functions_GUI();
+		fg1.initFromFile("function_file.txt");
+		fg1.drawFunctions("GUI_params.txt");
+
+	}
+
+	@Test
+	void testDrawFunctionsIntIntRangeRangeInt() throws IOException {
+		int w=1000, h=600, res=300;
+		Range rx = new Range(-10,10);
+		Range ry = new Range(-5,15);
+		Functions_GUI fg1=new Functions_GUI();
+		fg1.initFromFile("function_file.txt");
+		fg1.drawFunctions(w,h, rx, ry, res);
+
+	}
+	public static functions FunctionsFactory() {
+		functions ans = new Functions_GUI();
 		String s1 = "3.1 +2.4x^2 -x^4";
 		String s2 = "5 +2x -3.3x +0.1x^5";
 		String[] s3 = {"x +3","x -2", "x -4"};
@@ -78,28 +94,29 @@ class Functions_GUITest {
 		for(int i=1;i<s3.length;i++) {
 			cf3.mul(new Polynom(s3[i]));
 		}
-		
-		ComplexFunction cf = new ComplexFunction("plus", p1,p2);
+
+		ComplexFunction cf = new ComplexFunction(Operation.Plus, p1,p2);
 		ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
 		cf4.plus(new Monom("2"));
 		ans.add(cf.copy());
 		ans.add(cf4.copy());
 		cf.div(p1);
 		ans.add(cf.copy());
-		String s = cf.toString();
 		function cf5 = cf4.initFromString(s1);
 		function cf6 = cf4.initFromString(s2);
 		ans.add(cf5.copy());
 		ans.add(cf6.copy());
-		ComplexFunction max = new ComplexFunction(ans.get(0).copy());
-		ComplexFunction min = new ComplexFunction(ans.get(0).copy());
-		for(int i=1;i<ans.size();i++) {
-			max.max(ans.get(i));
-			min.min(ans.get(i));
+		Iterator<function> iter = ans.iterator();
+		function f = iter.next();
+		ComplexFunction max = new ComplexFunction(f);
+		ComplexFunction min = new ComplexFunction(f);
+		while(iter.hasNext()) {
+			f = iter.next();
+			max.max(f);
+			min.min(f);
 		}
 		ans.add(max);
-		ans.add(min);
-		
+		ans.add(min);		
 		return ans;
 	}
 }
